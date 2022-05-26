8f772bdb793801308600a36ee668563269accced
saurabh16107372 <95605060+saurabh1610737281@users.noreply.github.com>
2022-05-26 10:35:34 +0000

    Coding 26.05.2022

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -2,7 +2,12 @@ def computepay(h, r):
     if h>40:
           rate1= (r*1.5)*(h-40)
     return ((h-5)*r)+rate1
+  
 hrs = float(raw_input("Enter Hours:"))
 rate =float(raw_input('Enter rate:'))
-p =computepay(hrs, rate)
-print("Pay", p)
+if h<=40:
+  pay=hrs*rate
+  print(pay)
+else:
+   p =computepay(hrs, rate)
+   print("Pay", p)
